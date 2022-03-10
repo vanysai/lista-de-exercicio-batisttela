fd61b7c36ede09a76063aa559866038774fa4161
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:54:19 -0300

    Create divisao.js

---
+++ divisao.js
@@ -0,0 +1,7 @@
+const dividir = function(x,y){
+    let resultado = x / y 
+    return "Divisão: (" + x +") / (" + y +") = " + resultado
+}
+
+console.log(dividir(50,10))
+console.log(dividir(100, 2))
