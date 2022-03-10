05c89d502ce8fc7dfdbbd8467509bb76de5f5e2a
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:49:26 -0300

    Create subtracao.js

---
+++ subtracao.js
@@ -0,0 +1,7 @@
+const subtrair = function(x,y){
+    let resultado = x - y 
+    return "Subtração: (" + x + ") - (" + y + ") = " + resultado 
+}
+
+console.log(subtrair(40, 20))
+console.log(subtrair(50, 20))
