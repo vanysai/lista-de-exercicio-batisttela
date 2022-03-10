3ce1de5a67da6df33154a7e8e19133c7572c8257
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:20:26 -0300

    Create comidas.js

---
+++ comidas.js
@@ -0,0 +1,29 @@
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Comida 1"
+console.log(inteiro, real, cancelou, nome)
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Comida 2"
+console.log(inteiro, real, cancelou, nome)
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Comida 3"
+console.log(inteiro, real, cancelou, nome)
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Comida 4"
+console.log(inteiro, real, cancelou, nome)
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Comida 5"
+console.log(inteiro, real, cancelou, nome)
