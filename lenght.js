8c33c371ce3699bc5351b41c64c511a2d718e710
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 20:55:05 -0300

    Create lenght.js

---
+++ lenght.js
@@ -0,0 +1,10 @@
+
+let nome1 = "Isabella";
+let nome2 = "Miranda";
+let nome3 = "Carmen";
+let nome4 = "José";
+//nome 3 e nome 4
+
+const total = nome1.length + nome2.length + nome3.length + nome4.length;
+
+console.log(total)
