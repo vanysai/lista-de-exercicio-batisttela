bc57042cd73c89ae738087eac8beb6134aca45a2
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:48:21 -0300

    Create numeros.js

---
+++ numeros.js
@@ -0,0 +1,10 @@
+numero1= "1"
+numero2= "2"
+numero3= "3"
+numero4= "4"
+numero5= "5"
+numero6= "6"
+numero7= "7"
+numero8= "8"
+
+console.log(numero1, numero2, numero3, numero4, numero5, numero6,numero7, numero8)
