2a4bc3c7bed803cbff510e48c863d2cd94ff338f
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:16:34 -0300

    Create clientes.js

---
+++ clientes.js
@@ -0,0 +1,17 @@
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Cliente 1"
+console.log(inteiro, real, cancelou, nome)
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Cliente 2"
+console.log(inteiro, real, cancelou, nome)
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome = "Cliente 3"
+console.log(inteiro, real, cancelou, nome)
