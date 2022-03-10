354c349aab0ace6e53a1621593f1a21b4f45a063
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:56:22 -0300

    Create multiplicacao.js

---
+++ multiplicacao.js
@@ -0,0 +1,7 @@
+const multiplicar = function(x,y){
+    let resultado = x * y 
+    return "Multiplicação: (" + x +") * (" + y +") = " + resultado
+}
+
+console.log(multiplicar(10000,10))
+console.log(multiplicar(100456, 288))
