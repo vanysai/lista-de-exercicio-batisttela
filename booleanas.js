43a30dc7d7e30313abe86c799f0d66d71313f702
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:40:38 -0300

    Create booleanas.js

---
+++ booleanas.js
@@ -0,0 +1,13 @@
+nome1= "bernardo machado"
+nome2= "ana carol"
+nome3= "mariana fock"
+nome4= "enzo batista"
+nome5= "paula tejano"
+nome6= "paula dorneless"
+cancelou = true 
+lacrou = false
+virou = true
+tornou= false
+reprovou = false
+tirou = true
+console.log(cancelou, lacrou, virou, tornou, reprovou, tirou)
