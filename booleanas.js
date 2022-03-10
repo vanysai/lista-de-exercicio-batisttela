dba959824e9141d331841645a599a6cf9cada2d0
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-10 12:44:40 -0300

    Update booleanas.js

--- booleanas.js
+++ booleanas.js
@@ -4,10 +4,17 @@ nome3= "mariana fock"
 nome4= "enzo batista"
 nome5= "paula tejano"
 nome6= "paula dorneless"
+
 cancelou = true 
 lacrou = false
 virou = true
 tornou= false
 reprovou = false
 tirou = true
-console.log(cancelou, lacrou, virou, tornou, reprovou, tirou)
+
+console.log(nome1, cancelou)
+console.log(nome2, lacrou)
+console.log(nome3, virou)
+console.log(nome4, tornou)
+console.log(nome5, reprovou)
+console.log(nome6, tirou)
